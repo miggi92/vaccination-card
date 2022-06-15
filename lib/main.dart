0decae79c9c3fda09072bace542be769ed48a83b
Miguel Gebhardt <miguel@mgm12.de>
2022-06-15 22:45:50 +0200

    added firebase to everything

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
 
   FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
