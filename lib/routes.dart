846a2544e63513aedab9875633546bcac7898189
Miguel Gebhardt <miguel@mgm12.de>
2022-06-16 19:43:50 +0200

    added profile screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:vaccinationcard/screens/login.dart';
+import 'package:vaccinationcard/screens/profile.dart';
 
 import 'screens/home.dart';
 
 var appRoutes = {
   '/': (context) => const HomeScreen(),
-  '/login': (context) => LoginScreen()
+  '/login': (context) => const LoginScreen(),
+  '/profile': (context) => const ProfileScreen()
 };
---
+++ lib/screens/profile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ProfileScreen extends ConsumerWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profile')),
+    );
+  }
+}
