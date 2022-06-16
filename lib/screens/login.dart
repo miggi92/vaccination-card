0daa500be304418c585a7218708b33a18ca2d174
Miguel Gebhardt <miguel@mgm12.de>
2022-06-16 17:13:18 +0200

    added constr. key

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginScreen extends ConsumerWidget{
+  const LoginScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // TODO: implement build
