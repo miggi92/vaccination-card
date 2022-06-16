d716c6772f19a89857c8d3d095438082af702e5a
Miguel Gebhardt <miguel@mgm12.de>
2022-06-16 19:33:26 +0200

    added home tiles

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:vaccinationcard/services/auth.dart';
 
 class HomeScreen extends ConsumerWidget {
@@ -10,23 +11,49 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider);
     return Scaffold(
-      appBar: AppBar(title: Text('Main')),
-      body: Column(children: [
-        user.when(
-          data: (data) {
-            if (data != null) {
-              return Text(data.uid);
-            } else {
-              return ElevatedButton(
-                child: const Text('Login'),
-                onPressed: () => Navigator.pushNamed(context, '/login'),
-              );
-            }
-          },
-          error: (e, s) => Text('error'),
-          loading: () => Text('loading'),
-        )
-      ]),
+      appBar: AppBar(title: const Text('Vaccination card')),
+      body: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          padding: const EdgeInsets.all(20.0),
+          children: [
+            Container(
+              color: Theme.of(context).backgroundColor,
+              child: GridTile(
+                header: Center(
+                  child: Text('Info',
+                      style: Theme.of(context).textTheme.headline6),
+                ),
+                child: const Icon(FontAwesomeIcons.info),
+              ),
+            ),
+            user.when(
+              data: (data) {
+                if (data != null) {
+                  return GestureDetector(
+                    child: Container(
+                      color: Theme.of(context).backgroundColor,
+                      child: GridTile(
+                        header: Center(
+                          child: Text(data.displayName ?? 'Annonymous',
+                              style: Theme.of(context).textTheme.headline6),
+                        ),
+                        child: const Icon(FontAwesomeIcons.personRifle),
+                      ),
+                    ),
+                    onTap: () => Navigator.pushNamed(context, '/profile'),
+                  );
+                } else {
+                  return ElevatedButton(
+                    child: const Text('Login'),
+                    onPressed: () => Navigator.pushNamed(context, '/login'),
+                  );
+                }
+              },
+              error: (e, s) => const Text('error'),
+              loading: () => const Text('loading'),
+            )
+          ]),
     );
   }
 }
