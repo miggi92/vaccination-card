746a345753b7cc62dd93539965ea187604959468
Miguel Gebhardt <miguel@mgm12.de>
2022-06-16 18:48:26 +0200

    added darkmode

--- lib/theme.dart
+++ lib/theme.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 var appTheme = ThemeData(
-    fontFamily: GoogleFonts.nunito().fontFamily, primarySwatch: Colors.yellow);
+    brightness: Brightness.light,
+    fontFamily: GoogleFonts.nunito().fontFamily,
+    primarySwatch: Colors.yellow);
 
-var appDarkTheme = ThemeData(brightness: Brightness.dark);
+var appDarkTheme = ThemeData(
+    brightness: Brightness.dark,
+    fontFamily: GoogleFonts.nunito().fontFamily,
+    primarySwatch: Colors.yellow);
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_auth
 import firebase_core
 import firebase_crashlytics
 import path_provider_macos
+import shared_preferences_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
